28db55a2abbccbd9e7d4dbdbaa127515a2de6836
maouanaa <sofienmw@gmail.com>
2023-01-23 16:32:55 +0100

    button

---
+++ button/script.js
@@ -0,0 +1,12 @@
+function logout(id){
+    document.getElementById(id).innerText="logOut"
+}
+function hide(id){
+   document.getElementById(id).remove()
+   
+}
+function aler(id){
+ document.getElementById(id)
+ 
+alert("ninja was liked")
+}
