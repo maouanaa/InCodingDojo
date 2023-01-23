73eef4c7cb574c88798c06fd74b5b9aa1c5f0614
maouanaa <sofienmw@gmail.com>
2023-01-23 12:56:59 +0100

    loopchallenge

---
+++ javascript/loopchallenge.js
@@ -0,0 +1,40 @@
+for (var i=1; i <=20;i++)
+if(i % 2 != 0) {
+    console.log(i);
+}
+
+
+
+
+
+for (var i=100; i>=0;i--)
+if(i % 3 == 0) {
+    console.log(i);
+}
+
+
+for (var i=4; i>-4;i-=1.5)
+ {
+    console.log(i);
+}
+
+
+var sum=0
+function result(){
+for (i=1 ;i<=100; i++){
+sum+=i;
+}
+  
+}
+ result();
+console.log(sum);
+
+var product=1
+function result(){
+for (i=1 ;i<=12; i++){
+product*=i;
+}
+  
+}
+ result();
+console.log(product);
