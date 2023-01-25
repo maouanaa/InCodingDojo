b64f5afbfbe2dad84f2d71a49f5db5be5e294604
maouanaa <sofienmw@gmail.com>
2023-01-25 13:59:21 +0100

    exam 1

---
+++ exam1/script.js
@@ -0,0 +1,13 @@
+
+function aler() {
+    alert("Your cart is empty");
+}
+function over(e) {
+    e.src = "./image/succulents-2.jpg"
+}
+function out(e) {
+    e.src = "./image/succulents-1.jpg"
+}
+function remov(id) {
+    document.getElementById(id).remove()
+}
--- likes/seconds/script.js
+++ likes/seconds/script.js
@@ -1,3 +1,11 @@
 function plus(id){
     document.getElementById(id).innerText++
 }
+
+function over(e){
+    e.src = "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png"
+}
+
+function out(e){
+    e.src = "user-circle.png"
+}
